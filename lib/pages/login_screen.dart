0115002b12b9d201d1db1ee520e0f8944160e526
felipebritz <felipegustavobritz@gmail.com>
2020-11-28 19:09:45 -0300

    refactor: using scaffold background color

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -4,79 +4,75 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.grey[200],
-        child: Container(
-          padding: EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login to continue!',
-                style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[700]),
+        padding: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login to continue!',
+              style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700]),
+            ),
+            spacer(),
+            textFieldContainer(
+              iconData: Icons.person_outline,
+              description: 'Username',
+            ),
+            spacer(),
+            textFieldContainer(
+              iconData: Icons.lock_outline,
+              description: 'Password',
+            ),
+            spacer(),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  color: Colors.white,
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                Text(
+                  'Remember me',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+            spacer(),
+            Container(
+              width: double.infinity,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Colors.red[300],
+                    Colors.orange,
+                  ],
+                ),
               ),
-              spacer(),
-              textFieldContainer(
-                iconData: Icons.person_outline,
-                description: 'Username',
-              ),
-              spacer(),
-              textFieldContainer(
-                iconData: Icons.lock_outline,
-                description: 'Password',
-              ),
-              spacer(),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
+              child: Center(
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
                     color: Colors.white,
-                    child: Icon(
-                      Icons.check,
-                      color: Colors.grey[600],
-                    ),
+                    fontSize: 35,
                   ),
-                  Text(
-                    'Remember me',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ],
+                ),
               ),
-              spacer(),
-              Container(
-                width: double.infinity,
-                height: 80.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                      Colors.red[300],
-                      Colors.orange,
-                    ],
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
